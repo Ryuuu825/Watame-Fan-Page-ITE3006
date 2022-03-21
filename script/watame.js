02993ffb08d28bc6341b7e07b9c0c2512a625f9a
Ryuuu825 <85796869+Ryuuu825@users.noreply.github.com>
2022-03-21 14:51:08 +0800

    updated

---
+++ script/watame.js
@@ -0,0 +1,22 @@
+var is_clicked = false;
+
+function watame_say() {
+    var image = document.getElementById("watame_face_photo");
+    var text = document.getElementById("watame_sound_text");
+    var audio = document.getElementById("watame_sound");
+    var button = document.getElementById("btn_change_sound");
+
+    if (is_clicked) {
+        text.innerText = "baa?";
+        button.innerText = "Click me to listen";
+        image.src = "../source/watame.png";
+        is_clicked = false;
+    } else {
+        audio.volume = 0.1;
+        audio.play();
+        button.innerText = "back";
+        text.innerText = "悪くないよねぇ わため!";
+        image.src = "../source/face2.png";
+        is_clicked = true;
+    }
+}
