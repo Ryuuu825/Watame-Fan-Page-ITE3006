1fa91fdd731c022dba468dba0a68781fe573f7d5
Ben POON <jyuban@outlook.com>
2022-04-15 01:06:32 +0800

    真係超勁我同你講

--- script/main.js
+++ script/main.js
@@ -2,12 +2,11 @@ function $(Nid) {
 	return document.getElementById(Nid);
 }
 
-function changeIcon(x) {
-	x.classList.toggle("change");
+function changeIcon(obj) {
+	obj.classList.toggle("change");
 }
 
 function toggleNav() {
-	var winY = window.scrollY;
 	var isVisible = $("sideNav").style.visibility = $("sideNav").style.visibility === "visible" ? 'hidden' : 'visible';
 	$("sideNav").style.height = $("sideNav").style.height === "100%" ? '0' : '100%';
 	if (isVisible == "visible") {
