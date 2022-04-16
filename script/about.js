6914f410879a5779b92ea3003fbdb28ee0a69ed4
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2022-04-16 09:33:14 +0800

    fixed about pages

--- script/about.js
+++ script/about.js
@@ -1,21 +1,25 @@
 function checkForm() {
 	let canSend = true;
 
-	if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*$/.test(document.contact_form.email.value)) 
+	if (document.contact_form.message.value.length < 10)
 	{
-		document.getElementsByClassName('label')[0].style.display = 'flex';
-		document.contact_form.email.style.border = "3px solid #DC3232";
+		document.contact_form.message.style.border = '3px solid #DC3232';
+		document.contact_form.message.focus(); // focus on the field
+		document.getElementsByClassName('label')[2].style.display = 'flex';
 		canSend = false;
 	}
 	else 
 	{
-		document.getElementsByClassName('label')[0].style.display = 'none';
-		document.contact_form.email.style.border = "1px solid #ccc";
+		document.contact_form.message.style.border = '1px solid #F59A23';
+		document.getElementsByClassName('label')[2].style.display = 'none';
 	}
+
 	if (! /^[\+][\d]{3}[ ][\d]+/.test(document.contact_form.phone.value) )
 	{
 		document.getElementsByClassName('label')[1].style.display = 'flex';
 		document.contact_form.phone.style.border = '3px solid #DC3232';
+		document.contact_form.phone.focus(); // focus on the field
+
 		document.contact_form.phone.value = "";
 
 		canSend = false;
@@ -23,19 +27,43 @@ function checkForm() {
 	else 
 	{
 		document.getElementsByClassName('label')[1].style.display = 'none';
-		document.contact_form.phone.style.border = '1px solid #ccc';
+		document.contact_form.phone.style.border = '1px solid #F59A23';
 	}
-	if (document.contact_form.message.value.length < 10)
+
+	if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*$/.test(document.contact_form.email.value)) 
 	{
-		document.contact_form.message.style.border = '3px solid #DC3232';
-		document.getElementsByClassName('label')[2].style.display = 'flex';
+		document.getElementsByClassName('label')[0].style.display = 'flex';
+		document.contact_form.email.style.border = "3px solid #DC3232";
+		document.contact_form.email.focus(); // focus on the field
 		canSend = false;
 	}
-	else 
+	else  // correct 
 	{
-		document.contact_form.message.style.border = '1px solid #ccc';
-		document.getElementsByClassName('label')[2].style.display = 'none';
+		document.getElementsByClassName('label')[0].style.display = 'none';
+		document.contact_form.email.style.border = "1px solid #F59A23";
 	}
+
+
 	return canSend;
 }
 
+function changeBorderColor(target)
+{
+	target.style.border = '1px solid #F59A23';
+}
+
+function resetForm(result)
+{
+	if ( result )
+	{
+		document.getElementsByClassName('label')[0].style.display = 'none';
+		document.getElementsByClassName('label')[1].style.display = 'none';
+		document.getElementsByClassName('label')[2].style.display = 'none';
+	
+		document.contact_form.email.style.border = "1px solid #F59A23";
+		document.contact_form.phone.style.border = "1px solid #F59A23";
+		document.contact_form.message.style.border = "1px solid #F59A23";
+	}
+
+
+}
