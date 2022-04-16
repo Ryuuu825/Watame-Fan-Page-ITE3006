ac416293200ed8a6dc07a4eac018c0aa1a727298
Ben POON <jyuban@outlook.com>
2022-04-16 20:22:49 +0800

    Update about.js

--- script/about.js
+++ script/about.js
@@ -1,21 +1,17 @@
 function checkForm() {
 	let canSend = true;
 
-	if (document.contact_form.message.value.length < 10)
-	{
+	if (document.contact_form.message.value.length < 10) {
 		document.contact_form.message.style.border = '3px solid #DC3232';
 		document.contact_form.message.focus(); // focus on the field
 		document.getElementsByClassName('label')[2].style.display = 'flex';
 		canSend = false;
-	}
-	else 
-	{
+	} else {
 		document.contact_form.message.style.border = '1px solid #F59A23';
 		document.getElementsByClassName('label')[2].style.display = 'none';
 	}
 
-	if (! /^[\+][\d]{3}[ ][\d]+/.test(document.contact_form.phone.value) )
-	{
+	if (!/^[\+][\d]{3}[ ][\d]+/.test(document.contact_form.phone.value)) {
 		document.getElementsByClassName('label')[1].style.display = 'flex';
 		document.contact_form.phone.style.border = '3px solid #DC3232';
 		document.contact_form.phone.focus(); // focus on the field
@@ -23,50 +19,42 @@ function checkForm() {
 		document.contact_form.phone.value = "";
 
 		canSend = false;
-	}
-	else 
-	{
+	} else {
 		document.getElementsByClassName('label')[1].style.display = 'none';
 		document.contact_form.phone.style.border = '1px solid #F59A23';
 	}
 
-	if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*$/.test(document.contact_form.email.value)) 
-	{
+	if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*$/.test(document.contact_form.email.value)) {
 		document.getElementsByClassName('label')[0].style.display = 'flex';
 		document.contact_form.email.style.border = "3px solid #DC3232";
 		document.contact_form.email.focus(); // focus on the field
 		canSend = false;
-	}
-	else  // correct 
+	} else // correct 
 	{
 		document.getElementsByClassName('label')[0].style.display = 'none';
 		document.contact_form.email.style.border = "1px solid #F59A23";
 	}
-
+	if (canSend)
+		alert("Thank you");
 
 	return canSend;
 }
 
-function changeBorderColor(target)
-{
+function changeBorderColor(target) {
 	target.style.border = '1px solid #F59A23';
 }
 
-function resetForm(result)
-{
-	if ( result )
-	{
+function resetForm(result) {
+	if (result) {
 		document.getElementsByClassName('label')[0].style.display = 'none';
 		document.getElementsByClassName('label')[1].style.display = 'none';
 		document.getElementsByClassName('label')[2].style.display = 'none';
-	
+
 		document.contact_form.email.style.border = "1px solid #F59A23";
 		document.contact_form.phone.style.border = "1px solid #F59A23";
 		document.contact_form.message.style.border = "1px solid #F59A23";
 		return true;
-	}
-	else 
-	{
+	} else {
 		return false; // disable default reset button
 	}
 
